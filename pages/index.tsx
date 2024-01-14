39e2526a959dd5f33a166d3b6fd9619511032ac7
ewrvp7lv7 <vcxsw23@gmail.com>
2024-01-15 03:12:45 +0700

    useState added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useRouter } from "next/router";
-import * as Tabs from "@radix-ui/react-tabs";
 import { TabsDemo } from '../components/tabs';
 import { activeEra } from '../components/substrate';
 
 export default function Home() {
+  const [activeEraState, setActiveEraState] = React.useState(0);
+
   return (
     <div className="bg-white h-full p-2">
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
@@ -15,6 +15,7 @@ export default function Home() {
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
         </svg>
         Connected to <span className="bg-blue-100 font-semibold me-2 px-2.5 py-0.5 ms-2">wss://rpc.polkadot.io</span>
+        Current era: <span className="bg-blue-100 font-semibold me-2 px-2.5 py-0.5 ms-2">{activeEraState}</span>
       </h5>
       <TabsDemo />
     </div>
