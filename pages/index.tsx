2a5edb8f8c1ed6d3d0e377f586918276e4da42aa
ewrvp7lv7 <vcxsw23@gmail.com>
2024-01-15 03:21:58 +0700

    Active era renamed

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
         </svg>
         Connected to <span className="bg-blue-100 font-semibold me-2 px-2.5 py-0.5 ms-2">wss://rpc.polkadot.io</span>
-        Current era: <span className="bg-blue-100 font-semibold me-2 px-2.5 py-0.5 ms-2">{activeEraState}</span>
+        Active era: <span className="bg-blue-100 font-semibold me-2 px-2.5 py-0.5 ms-2">{activeEraState}</span>
       </h5>
       <TabsDemo />
     </div>
